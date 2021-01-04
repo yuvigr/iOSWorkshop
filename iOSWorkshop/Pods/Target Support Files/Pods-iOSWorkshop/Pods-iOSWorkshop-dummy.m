80d776a591ccc42d69612bfbd594a932c6e91023
Yoav Gross <yoav.gross@autodesk.com>
2021-01-04 12:46:10 +0200

    first commit

---
+++ iOSWorkshop/Pods/Target Support Files/Pods-iOSWorkshop/Pods-iOSWorkshop-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSWorkshop : NSObject
+@end
+@implementation PodsDummy_Pods_iOSWorkshop
+@end
---
+++ iOSWorkshop/iOSWorkshop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSWorkshop
+//
+//  Created by Yoav Gross on 04/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
